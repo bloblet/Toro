97ad016f31400683d2468610341497ba491f76b9
piecubed <40523150+piecubed@users.noreply.github.com>
2020-04-08 20:37:46 -0400

    yay

---
+++ lib/homeCard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Container(
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          color: Colors.black12,
+        ),
+        child: Container(
+          height: MediaQuery.of(context).size.height / 6,
+          child: ListTile(
+            title: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[Text('Current Portfolio Value')],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
