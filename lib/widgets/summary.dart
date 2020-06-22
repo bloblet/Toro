c12ae5c72228100d61f464973c9ed3d69269eb46
enrval <evguidesrc@gmail.com>
2020-06-22 12:38:16 -0400

    made some yayness

--- lib/widgets/summary.dart
+++ lib/widgets/summary.dart
@@ -13,7 +13,7 @@ class Summary extends StatelessWidget {
             elevation: 0,
             floating: true,
             snap: true,
-            expandedHeight: 200,
+            expandedHeight: 50,
             centerTitle: true,
             pinned: true,
             leading: IconButton(
@@ -27,11 +27,84 @@ class Summary extends StatelessWidget {
               },
             ),
             // Pretend this is a normal appbar
+            title: Text(
+              'Summary'
+            ),
           ),
           SliverList(
             delegate: SliverChildListDelegate(
               [
-                // and this is like a listView
+                Container(
+                  padding: EdgeInsets.all(20),
+                  color: Colors.grey[100],
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Your Balance Details",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "\$1,234,567.27",
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      ),
+                      Text(
+                        "Today's gain/loss",
+                        style: TextStyle(
+                          color: Colors.green[700],//balance going up ? Colors.green[700] : Colors.red[700],
+                        ),
+                      ),
+                      Text(
+                        "+\$12,345.34",
+                        style: TextStyle(
+                          color: Colors.green[700],//balance going up ? Colors.green[700] : Colors.red[700],
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        "As of 12:10PM ET 6/22/20",
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 12,
+                          fontWeight: FontWeight.w200,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  color: Colors.grey[100],
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "Your Balance History",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      //add the chart here.
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
