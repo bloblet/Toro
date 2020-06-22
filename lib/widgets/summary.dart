f7e31e3a758458e170089ebfcdb5d44c0b6ca4a2
piecubed <40523150+piecubed@users.noreply.github.com>
2020-06-22 12:05:45 -0400

    yay

--- lib/widgets/summary.dart
+++ lib/widgets/summary.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'tabScaffold.dart';
+import 'zoomScaffold.dart';
 
 class Summary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TabScaffold(
-          body: Container(
-        child: Text('Testing coolness'),
+      body: (zoomContext) => CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            elevation: 0,
+            floating: true,
+            snap: true,
+            expandedHeight: 200,
+            centerTitle: true,
+            pinned: true,
+            leading: IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Provider.of<MenuController>(zoomContext, listen: false)
+                  ..toggle();
+              },
+            ),
+            // Pretend this is a normal appbar
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                // and this is like a listView
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
